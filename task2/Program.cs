a85ab2843e0aef176f61f57dfc92ff2295f55cb0
Anastasiia <kariakina.nastia@example.com>
2022-08-10 12:58:53 +0300

    add Task 2 and execution screenshot

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел.*/
+Console.WriteLine ("Введите три числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine()); 
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int Max = number1;
+if (number2 > Max) Max = number2;
+if (number3 > Max) Max = number3; 
+Console.WriteLine("Max = "+ Max);
